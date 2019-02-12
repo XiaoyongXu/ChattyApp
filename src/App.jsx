5d88f03747cfa2d61d6b8760af96a453eea97207
XiaoyongXu <xxy1994212@gmail.com>
2019-02-12 03:00:49 +0000

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // this is the *only* time you should assign directly to state:
+    this.state = {
+      
+      currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: 
+      [
+        {
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+        },
+        {
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ],
+      loading: true
+    
+    };
+  }
+  componentDidMount() {
+    // After 3 seconds, set `loading` to false in the state.
+    setTimeout(() => {
+      this.setState({loading: false}); // this triggers a re-render!
+    }, 2000)
+  }
   render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>
+    } else {
     return (
-      <h1>Hello React :)</h1>
-    );
+      <h1>{this.state.currentUser.name}</h1>
+      );
+    }
   }
 }
 export default App;
